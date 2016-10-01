ec0d9b8da899ae9f4690f161e5cb88f35b854fbc
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2016-10-01 15:41:25 +0100

    server

---
+++ Conexao_S_C/src/conexao_s_c/Servidor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexao_s_c;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Filipe Oliveira
+ */
+public class Servidor {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.pintln("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+        
+        Scanner s = new Scanner(cliente.getInputStream());
+        while(s.hasNextLine()){
+            System.out.println(s.nextLine());
+        }
+        
+        s.close();
+        servidor.close();
+        cliente.close();
+    }
+}
