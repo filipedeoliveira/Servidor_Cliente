ff4ccafe4dca51d91b803bf994691a3227c228cd
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2016-10-01 17:16:15 +0100

    cnx

--- Conexao_S_C/src/conexao_s_c/Servidor.java
+++ Conexao_S_C/src/conexao_s_c/Servidor.java
@@ -6,8 +6,12 @@
 package conexao_s_c;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,27 +21,62 @@ import java.util.Scanner;
 public class Servidor {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket servidor = new ServerSocket(12345);
+        //inicia servidor
+        new Servidor(12345).executa();
+    }
+
+    private int porta;
+    private List<PrintStream> clientes;
+
+    public Servidor(int porta) {
+        this.porta = porta;
+        this.clientes = new ArrayList<PrintStream>();
+    }
+
+    public void executa() throws IOException {
+        ServerSocket servidor = new ServerSocket(this.porta);
         System.out.println("Porta 12345 aberta!");
 
-        Socket cliente = servidor.accept();
+        while (true) {
+            //aceita cliente
+            Socket cliente = servidor.accept();
+            System.out.println("Nova Conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
-        //Objecto para tratar da conexão
-        TratamentoClass tratamento = new TratamentoClass(cliente);
-        
-        Thread t = new Thread(tratamento);
-        
-        t.start();
-        
-        System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+            //adicionar saida do cliente À lista
+            PrintStream ps = new PrintStream(cliente.getOutputStream());
+            this.clientes.add(ps);
 
-        Scanner s = new Scanner(cliente.getInputStream());
-        while (s.hasNextLine()) {
-            System.out.println(s.nextLine());
+            //tratar de clinete com nova thread
+            TrataCliente tc = new TrataCliente(cliente.getInputStream(), this);
+            new Thread(tc).start();
         }
+    }
 
+    public void distribuiMensagem(String msg) {
+        //enviar mensagen para todos
+        for (PrintStream cliente : this.clientes) {
+            cliente.println(msg);
+        }
+    }
+
+}
+
+class TrataCliente implements Runnable {
+
+    private InputStream cliente;
+    private Servidor servidor;
+
+    public TrataCliente(InputStream cliente, Servidor servidor) {
+        this.cliente = cliente;
+        this.servidor = servidor;
+    }
+
+    public void run() {
+        //quando chegada de msg, distribui para todos
+        Scanner s = new Scanner(this.cliente);
+        while (s.hasNextLine()) {
+            servidor.distribuiMensagem(s.nextLine());
+        }
         s.close();
-        servidor.close();
-        cliente.close();
     }
 }
